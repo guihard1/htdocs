af9d998c7084425922d7af31afe85a3fa234d742
Luiz Guilherme Rodrigues <91224521+guihard1@users.noreply.github.com>
2025-09-15 08:24:23 -0300

    inicio modulo2
    Algumas atualizações da materia do modulo 2

---
+++ modulo2/modulo2/aula1/index.php
@@ -0,0 +1,27 @@
+<?php
+    $nome = 'Luiz';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <h2>
+            Eu sou
+            <?php
+            echo $nome;
+            ?>
+        </h2>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
